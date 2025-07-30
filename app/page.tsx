355720ebae1b67b50c71b97e3903a66e71020c7b
nitesh sikarwar <nsikarwar@hotbitinfotech.com>
2025-07-30 17:58:10 +0530

    feature/ delete page.tsx

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import LineChart from '@/components/organsims/LineChart';
-
-export default function Home() {
-  return (
-    <div className="m-2 w-full h-full">
-      <LineChart />
-    </div>
-  );
-}
