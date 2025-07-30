7a62006bc9f0290db6b93df4decdbba37f2b3afb
nitesh sikarwar <nsikarwar@hotbitinfotech.com>
2025-07-30 17:34:41 +0530

    updated with main

--- components/DashboardClient.tsx
+++ components/DashboardClient.tsx
@@ -2,6 +2,7 @@
 
 import { DashboardApiResponse } from "@/lib/types/types";
 import { useEffect, useState } from "react";
+import Map from "./organsims/Map";
 
 type Props = {
   initialData: DashboardApiResponse;
@@ -31,6 +32,7 @@ export default function DashboardClient({ initialData }: Props) {
       <main className="flex-1 p-4 space-y-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           data
+          <Map />
         </div>
       </main>
     </div>
