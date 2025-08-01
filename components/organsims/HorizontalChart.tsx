a6226f15ce13eb874de3e260047d8b9b60f22107
Tusharhotbit@123 <tsharma@hotbitinfosoft.com>
2025-08-01 17:46:02 +0530

    api integration completed

--- components/organsims/HorizontalChart.tsx
+++ components/organsims/HorizontalChart.tsx
@@ -10,15 +10,106 @@ import {
   Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
-import {
-  horizontalChartData,
-  horizontalChartOptions,
-  topLabelPlugin,
-} from '@/lib/utils/horizontalchart';
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-export default function HorizontalBarChart() {
+interface horizontalChartProps {
+  data: {
+    labels: string[];
+    data: number[];
+  };
+}
+
+export default function HorizontalBarChart({ data }: horizontalChartProps) {
+  const weeks = data.labels;
+  const fixedData = data.data;
+
+  function getCssVar(name: string): string {
+    return getComputedStyle(document.documentElement).getPropertyValue(name).trim();
+  }
+
+  const horizontalChartData = {
+    labels: weeks,
+    datasets: [
+      {
+        label: 'Fixed Dataset',
+        data: fixedData,
+        backgroundColor: () => {
+          return getCssVar('--bar');
+        },
+        borderRadius: 8,
+        borderSkipped: false,
+      },
+    ],
+  };
+
+  const topLabelPlugin = {
+    id: 'topLabelPlugin',
+    afterDatasetsDraw(chart: any) {
+      const { ctx } = chart;
+
+      chart.data.datasets.forEach((dataset: any, i: number) => {
+        const meta = chart.getDatasetMeta(i);
+        meta.data.forEach((bar: any, index: number) => {
+          const value = dataset.data[index];
+          const styles = getCssVar('--text');
+          ctx.fillStyle = styles;
+          ctx.font = 'bold 12px sans-serif';
+          ctx.textAlign = 'left';
+          ctx.textBaseline = 'middle';
+          ctx.fillText(value, bar.x + 10, bar.y);
+        });
+      });
+    },
+  };
+
+  const horizontalChartOptions = {
+    indexAxis: 'y',
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: { display: false, position: 'top' },
+      title: {
+        display: false,
+        text: 'Monthly Data (Jan - Jun)',
+      },
+    },
+    scales: {
+      x: {
+        grid: { display: true, drawBorder: false },
+        border: { display: false },
+        ticks: {
+          color: () => {
+            return getCssVar('--text');
+          },
+          stepSize: 400,
+          display: false,
+          font: {
+            size: 14,
+            weight: 'bold',
+          },
+        },
+      },
+      y: {
+        reverse: true,
+        beginAtZero: true,
+        grid: { display: false, drawBorder: false },
+        ticks: {
+          color: () => {
+            return getCssVar('--text');
+          },
+          stepsize: 100,
+          display: true,
+          font: {
+            size: 14,
+            weight: 'bold',
+          },
+        },
+        border: { display: false },
+      },
+    },
+  } as Record<string, any>;
+
   return (
     <div className="w-full max-w-xl mx-auto p-4 h-full bg-card-bg pb-10!">
       <Bar options={horizontalChartOptions} data={horizontalChartData} plugins={[topLabelPlugin]} />
--- components/organsims/LineChart.tsx
+++ components/organsims/LineChart.tsx
@@ -12,7 +12,7 @@ import {
   Filler,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import { lineData, lineOptions } from '@/lib/utils/linechart';
+
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -24,7 +24,130 @@ ChartJS.register(
   Filler
 );
 
-export default function LineChart() {
+interface SalesOverTime {
+  labels: string[]; // e.g., ['2025-08-01', '2025-07-31', ...]
+  data: number[]; // e.g., [105, 165, 50, ...]
+}
+
+export default function LineChart({ data, labels }: SalesOverTime) {
+  const lineLabels = labels.map(label => label.split('-')[2]).reverse();
+  const lineSales = data.map(item => item).reverse();
+  // const lineSales2 = [100, 300, 400, 250, 500, 650];
+
+  const lineData = {
+    labels: lineLabels,
+    datasets: [
+      // {
+      //   label: 'Monthly Sales',
+      //   data: lineSales2,
+      //   fill: true,
+      //   borderColor: getCssVar('--linechart-line'),
+      //   backgroundColor: (ctx: any) => {
+      //     const from = getCssVar('--linegradient-under-from').trim();
+      //     const to = getCssVar('--linegradient-under-to').trim();
+
+      //     if (!from || !to) {
+      //       console.warn('Colors not available yet, returning transparent');
+      //       return 'transparent';
+      //     }
+
+      //     const gradient = ctx.chart.ctx.createLinearGradient(0, 0, 0, 300);
+      //     gradient.addColorStop(0, from);
+      //     gradient.addColorStop(1, to);
+      //     return gradient;
+      //   },
+      //   tension: 0.4,
+      //   pointBackgroundColor: '#07557C',
+      // },
+      {
+        label: 'Monthly Sales',
+        data: lineSales,
+        fill: true,
+        borderColor: '#07557C',
+        backgroundColor: (ctx: any) => {
+          const from = getCssVar('--linegradient-from')?.trim();
+          const to = getCssVar('--linegradient-to')?.trim();
+
+          if (!from || !to) {
+            console.warn('Line gradient colors not available, returning transparent');
+            return 'transparent';
+          }
+
+          console.log(from, to, 'this is from to');
+
+          const gradient = ctx.chart.ctx.createLinearGradient(0, 0, 0, 300);
+          gradient.addColorStop(0, from);
+          gradient.addColorStop(1, to);
+          return gradient;
+        },
+        tension: 0.4,
+        pointBackgroundColor: '#07557C',
+      },
+    ],
+  };
+
+  function getCssVar(name: string): string {
+    return getComputedStyle(document.documentElement).getPropertyValue(name).trim();
+  }
+
+  const lineOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false,
+        position: 'top' as const,
+      },
+      title: {
+        display: false,
+        text: 'Sales Trend (Jan - Jun)',
+      },
+    },
+    scales: {
+      x: {
+        type: 'category',
+        grid: {
+          display: false,
+          drawBorder: false,
+        },
+        border: {
+          display: false,
+        },
+        ticks: {
+          color: () => {
+            return getCssVar('--text');
+          },
+          font: {
+            size: 14,
+            weight: 'bold ',
+          },
+        },
+      },
+      y: {
+        type: 'linear',
+        grid: {
+          display: false,
+          drawBorder: false,
+        },
+        min: 0,
+        max: 300,
+        ticks: {
+          color: () => {
+            return getCssVar('--text');
+          },
+          stepSize: 200,
+          font: {
+            size: 14,
+            weight: 'bold',
+          },
+        },
+        border: {
+          display: false,
+        },
+      },
+    } as Record<string, any>,
+  };
+
   return (
     <div className="h-[350px] bg-card-bg">
       <Line data={lineData} options={lineOptions} />
